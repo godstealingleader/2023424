5da2524df8a483d36921a6e52622f33dd12eb8cf
godstealingleader <107918254+godstealingleader@users.noreply.github.com>
2023-05-03 19:24:54 +0800

    Add files via upload

---
+++ 2023424.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <array>
+//
+//int main()
+//{
+//	using namespace std;
+//	array<long double, 100 >number= {0};
+//	long double sum = 1;
+//	for (int i = 0; i < 100; i++)
+//	{
+//		number[0 + i] = i + 1;
+//	}
+//	for (long double& i : number)
+//	{
+//		sum = i * sum;
+//	}
+//	cout << sum;
+//	return 0;
+//}
